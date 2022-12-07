5df4fa95aae371785cad0549d7606daa9b260405
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-07 17:10:53 +0530

    Length of line of co-ordinates

--- src/com/linecomperision/LineCompresion.java
+++ src/com/linecomperision/LineCompresion.java
@@ -4,5 +4,11 @@ public class LineCompresion {
     public static void main(String[] args) {
         System.out.println("Start with Displaying\n" +
                 "Welcome to Line Comparison Computation Program");
+
+        int x1,x2,y1,y2;
+        double dis;
+        x1=1;y1=1;x2=4;y2=4;
+        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Length of line of coordinates"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
     }
 }
