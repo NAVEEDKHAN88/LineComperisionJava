43e5d32f4d5f2383a143b5f8c735fd5c2f055f3e
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-07 17:25:58 +0530

    Length of two lines are equal or not

--- src/com/linecomperision/LineCompresion.java
+++ src/com/linecomperision/LineCompresion.java
@@ -6,9 +6,21 @@ public class LineCompresion {
                 "Welcome to Line Comparison Computation Program");
 
         int x1,x2,y1,y2;
-        double dis;
+        int p1,q1,p2,q2;
+        double dis,dis1;
         x1=1;y1=1;x2=4;y2=4;
+        p1=1;q1=1;p2=4;q2=4;
         dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        dis1=Math.sqrt((p2-p1)*(p2-p1) + (q2-q1)*(q2-q1));
         System.out.println("Length of line of coordinates"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
-    }
+        System.out.println("Length of line of coordinates"+"("+p1+","+q1+"),"+"("+p2+","+q2+")===>"+dis1);
+        if (dis == dis1)
+        {
+            System.out.println("Both lines are equale");
+        }
+        else
+        {
+            System.out.println("Both lines are not equale");
+        }
+}
 }
