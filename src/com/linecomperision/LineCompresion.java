8c13f49838f7a915316e811112498124d5002413
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-07 17:04:16 +0530

    welcome to Line Comparision

---
+++ src/com/linecomperision/LineCompresion.java
@@ -0,0 +1,8 @@
+package com.linecomperision;
+
+public class LineCompresion {
+    public static void main(String[] args) {
+        System.out.println("Start with Displaying\n" +
+                "Welcome to Line Comparison Computation Program");
+    }
+}
